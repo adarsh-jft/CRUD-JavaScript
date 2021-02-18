4a72d1328bd5ff3a2b96593067f9a28a34d9a922
Adarsh Pal <adarsh.pal@jellyfishtechnologies.com>
2021-02-18 18:23:43 +0530

    changed

--- crud.js
+++ crud.js
@@ -1,4 +1,3 @@
-// var count = 0;
 var selectedRow = null
 var x = 1;
 
@@ -6,17 +5,7 @@ function tableRowCount() {
     x = document.getElementById("details").rows.length;
 }
 
-// function myFunction(x) {
-//     var x;
-//     if (x == 1) {
-//         count = count + 1;
-//     }
-//     if (count == 1) {
-//         document.getElementById("add").style.backgroundColor = "red";
-//     } else if (count == 2) {
-//         document.getElementById("add").style.backgroundColor = "green";
-//     }
-// }
+
 
 function onFormSubmit() {
     var formData = readFormData();
@@ -31,20 +20,11 @@ function onFormSubmit() {
 
 function readFormData() {
     var formData = {};
-    localStorage.setItem("id", x);
     formData["id"] = x;
     formData["fname"] = document.getElementById("fname").value;
-    var y = formData["fname"];
-    localStorage.setItem("fname", y);
     formData["lname"] = document.getElementById("lname").value;
-    var z = formData["lname"];
-    localStorage.setItem("lname", z);
     formData["age"] = document.getElementById("age").value;
-    var a = formData["age"];
-    localStorage.setItem("age", a);
     formData["pno"] = document.getElementById("pno").value;
-    var b = formData["pno"];
-    localStorage.setItem("pno", b);
     return formData;
 
 
@@ -65,12 +45,11 @@ function addRecord(data) {
     cell5 = newRow.insertCell(4);
     cell5.innerHTML = data.pno;
     cell6 = newRow.insertCell(5);
-    cell6.innerHTML = `<a href = "" onclick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</a> <a href ="" onClick="onDelete(this)">Delete</a>`;
+    cell6.innerHTML = `<button  onclick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 
 }
 
 function resetForm() {
-    // document.getElementById("id").value = "";
     document.getElementById("fname").value = "";
     document.getElementById("lname").value = "";
     document.getElementById("age").value = "";
@@ -80,7 +59,6 @@ function resetForm() {
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    //document.getElementById("id").value = seletedRow.cells[0].innerHTML;
     document.getElementById("fname").value = selectedRow.cells[1].innerHTML;
     document.getElementById("lname").value = selectedRow.cells[2].innerHTML;
     document.getElementById("age").value = selectedRow.cells[3].innerHTML;
@@ -88,7 +66,6 @@ function onEdit(td) {
 }
 
 function updateRecords(formData) {
-    //selectedRow.cells[0].innerHTML = formData.id;
     selectedRow.cells[1].innerHTML = formData.fname;
     selectedRow.cells[2].innerHTML = formData.lname;
     selectedRow.cells[3].innerHTML = formData.age;
@@ -104,15 +81,40 @@ function onDelete(td) {
 
 }
 
-// function fetchData() {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//         .then(res => res.json())
-//         .then(data => {
-//             window.localStorage.setItem("name", JSON.stringify(data.name));
-//             console.log(data);
-//             // console.log(JSON.parse(window.localStorage.getItem("user")));
+function fetchData() {
+    fetch("https://jsonplaceholder.typicode.com/users").then(
+        res => {
+            res.json().then(
+                data => {
+                    console.log(data);
 
-//         })
-// }
+                    if (data.length > 0) {
+                        var temp = "";
+                        x = data.length + 1;
+                        if (!localStorage.getItem("data")) {
+                            localStorage.setItem("data", data)
+                        } else {
+                            let newData = localStorage.getItem("data")
+                            let newData2 = JSON.parse(newData)
+                            for (let i = 0; i < data.length; i++) {
+                                temp += "<tr>";
+                                temp += "<td>" + newData2[i].id + "</td>";
+                                temp += "<td>" + newData2[i].name + "</td>";
+                                temp += "<td>" + newData2[i].username + "</td>";
+                                temp += "<td>" + newData2[i].email + "</td>";
+                                temp += "<td>" + newData2[i].phone + "</td>";
+                                temp += '<td><button onclick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</button> <button onClick="onDelete(this)">Delete</button></td></tr>';
+                            }
+                        }
+                        document.getElementById("data").innerHTML = temp;
+                        let myObj = JSON.stringify(data);
+                        localStorage.setItem("data", myObj);
+                    }
+                }
+            )
+        }
+    )
 
-// fetchData();
+}
+
+fetchData();
