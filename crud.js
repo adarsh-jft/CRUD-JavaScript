a0775a99daed812420122fc4143c3b4def4e0b58
Adarsh Pal <adarsh.pal@jellyfishtechnologies.com>
2021-02-22 16:04:45 +0530

    done

--- crud.js
+++ crud.js
@@ -3,31 +3,53 @@ var x = 1;
 
 function tableRowCount() {
     x = document.getElementById("details").rows.length;
+    document.getElementById('rowCount').innerHTML = "Total Rows " + (x - 1);
 }
 
-
-
 function onFormSubmit() {
-    var formData = readFormData();
-    if (selectedRow == null) {
-        addRecord(formData);
+    var validateName = document.getElementById("name").value;
+    if (validateName != "") {
+        var formData = readFormData();
+        if (selectedRow == null) {
+            addRecord(formData);
+        } else {
+            updateRecords(formData);
+        }
+        resetForm();
+        tableRowCount();
     } else {
-        updateRecords(formData);
+        alert('Enter Your Name')
     }
-    resetForm();
-    tableRowCount();
+
 }
 
 function readFormData() {
-    var formData = {};
-    formData["id"] = x;
-    formData["fname"] = document.getElementById("fname").value;
-    formData["lname"] = document.getElementById("lname").value;
-    formData["age"] = document.getElementById("age").value;
-    formData["pno"] = document.getElementById("pno").value;
-    return formData;
-
-
+    formData = {
+        id: x,
+        name: document.getElementById("name").value,
+        username: document.getElementById("uname").value,
+        phone: document.getElementById("email").value,
+        email: document.getElementById("pno").value
+    }
+    if (!localStorage.getItem("data")) {
+        localStorage.setItem('data', data)
+    } else {
+        let stored = localStorage.getItem('data')
+        let stored_obj = JSON.parse(stored);
+        // var stored=JSON.parse(localStorage.getItem("data"))
+        //console.log(stored_obj)
+        stored_obj.push(formData)
+        console.log(stored_obj)
+        let myObj = JSON.stringify(stored_obj)
+        localStorage.setItem("data", myObj)
+    }
+    var FormData = {};
+    FormData["id"] = x;
+    FormData["name"] = document.getElementById("name").value;
+    FormData["uname"] = document.getElementById("uname").value;
+    FormData["email"] = document.getElementById("email").value;
+    FormData["pno"] = document.getElementById("pno").value;
+    return FormData;
 }
 
 function addRecord(data) {
@@ -37,48 +59,50 @@ function addRecord(data) {
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.id;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.fname;
+    cell2.innerHTML = data.name;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.lname;
+    cell3.innerHTML = data.uname;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.age;
+    cell4.innerHTML = data.email;
     cell5 = newRow.insertCell(4);
     cell5.innerHTML = data.pno;
     cell6 = newRow.insertCell(5);
-    cell6.innerHTML = `<button  onclick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+    cell6.innerHTML = `<button  onclick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</button> <button  data-toggle='modal' data-target='#delete' onClick='onDelete2(this)'>Delete</button>`;
 
 }
 
 function resetForm() {
-    document.getElementById("fname").value = "";
-    document.getElementById("lname").value = "";
-    document.getElementById("age").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("uname").value = "";
+    document.getElementById("email").value = "";
     document.getElementById("pno").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fname").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("lname").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("age").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("id").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("name").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("uname").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[3].innerHTML;
     document.getElementById("pno").value = selectedRow.cells[4].innerHTML;
 }
 
 function updateRecords(formData) {
-    selectedRow.cells[1].innerHTML = formData.fname;
-    selectedRow.cells[2].innerHTML = formData.lname;
-    selectedRow.cells[3].innerHTML = formData.age;
+    selectedRow.cells[1].innerHTML = formData.name;
+    selectedRow.cells[2].innerHTML = formData.uname;
+    selectedRow.cells[3].innerHTML = formData.email;
     selectedRow.cells[4].innerHTML = formData.pno;
 }
 
-function onDelete(td) {
-    if (confirm('Are you sure you want to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("details").deleteRow(row.rowIndex);
-        resetForm();
-    }
+function onDelete() {
+    document.getElementById("details").deleteRow(row.rowIndex);
+    tableRowCount();
 
+}
+
+function onDelete2(td) {
+    row = td.parentElement.parentElement;
 }
 
 function fetchData() {
@@ -86,7 +110,7 @@ function fetchData() {
         res => {
             res.json().then(
                 data => {
-                    console.log(data);
+                    // console.log(data);
 
                     if (data.length > 0) {
                         var temp = "";
@@ -104,7 +128,7 @@ function fetchData() {
                                 temp += "<td>" + newData2[i].username + "</td>";
                                 temp += "<td>" + newData2[i].phone + "</td>";
                                 temp += "<td>" + newData2[i].email + "</td>";
-                                temp += "<td><button class='btn btn-warning' onClick='onEdit(this)' data-toggle='modal' data-target='#exampleModal'>Edit</button> <button class='btn btn-danger' onClick='onDelete(this)'>Delete</button>";
+                                temp += "<td><button onClick='onEdit(this)' data-toggle='modal' data-target='#exampleModal'>Edit</button> <button data-toggle='modal' data-target='#delete' onClick='onDelete2(this)'>Delete</button>";
                             }
                         }
                         document.getElementById("data").innerHTML = temp;
